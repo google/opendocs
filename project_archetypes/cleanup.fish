1f9d11c9884336a5ef37b4d93f277a6a39beda5f
Daniel D. Beck <daniel@ddbeck.com>
2025-04-22 16:39:35 +0200

    Clean up files selectively

--- project_archetypes/cleanup.fish
+++ project_archetypes/cleanup.fish
@@ -1,26 +1,35 @@
 #!/usr/bin/env fish
 
-for md in ./*.md
+set mdfiles
+if test (count $argv) -eq 0
+    set mdfiles ./*.md
+else
+    set mdfiles $argv
+end
+
+for md in $mdfiles
     pandoc --wrap=preserve --from commonmark+smart --to=commonmark-smart $md | sponge $md
 end
-npx prettier --write ./*.md
+npx prettier --write $mdfiles
 
-sd '\[The Audit\]\(.+?\)' '[The Audit](./audit.md)' ./*.md
-sd '\[The Edit\]\(.+?\)' '[The Edit](./edit.md)' ./*.md
-sd '\[The Factory\]\(.+?\)' '[The Factory](./factory.md)' ./*.md
-sd '\[The Manual\]\(.+?\)' '[The Manual](./manual.md)' ./*.md
-sd '\[The Migration\]\(.+?\)' '[The Migration](./migration.md)' ./*.md
-sd '\[The Prototype\]\(.+?\)' '[The Prototype](./prototype.md)' ./*.md
-sd '\[The Rules\]\(.+?\)' '[The Rules](./rules.md)' ./*.md
-sd '\[The Study\]\(.+?\)' '[The Study](./study.md)' ./*.md
+sd '\[The Audit\]\(.+?\)' '[The Audit](./audit.md)' $mdfiles
+sd '\[The Edit\]\(.+?\)' '[The Edit](./edit.md)' $mdfiles
+sd '\[The Factory\]\(.+?\)' '[The Factory](./factory.md)' $mdfiles
+sd '\[The Manual\]\(.+?\)' '[The Manual](./manual.md)' $mdfiles
+sd '\[The Migration\]\(.+?\)' '[The Migration](./migration.md)' $mdfiles
+sd '\[The Prototype\]\(.+?\)' '[The Prototype](./prototype.md)' $mdfiles
+sd '\[The Rules\]\(.+?\)' '[The Rules](./rules.md)' $mdfiles
+sd '\[The Show\]\(.+?\)' '[The Show](./show.md)' $mdfiles
+sd '\[The Study\]\(.+?\)' '[The Study](./study.md)' $mdfiles
+sd '\[The Widget\]\(.+?\)' '[The Widget](./widget.md)' $mdfiles
 
 sd -F \
     'Most projects won’t do all of these steps.
 Choose the ones that make the most sense for your software project, available skills and resources, and timeline.' \
     'You probably won’t do all of these steps.
 Choose the ones that make the most sense for your goals, available skills and resources, and timeline.' \
-    ./*.md
+    $mdfiles
 
 sd --flags=i -- '^- support roles.*$' \
     '- Support roles, such as community managers, foundation leaders, and fiscal and other sponsors (and sponsor staff, such as finance, recruiters, sysadmins, or legal)' \
-    ./*.md
+    $mdfiles
