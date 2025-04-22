f3d5a56488a936ca87b27b4a20c6122cc92eac48
Daniel D. Beck <daniel@ddbeck.com>
2025-04-22 17:45:03 +0200

    Catch renamed archetypes

--- project_archetypes/cleanup.fish
+++ project_archetypes/cleanup.fish
@@ -17,11 +17,15 @@ sd '\[The Edit\]\(.+?\)' '[The Edit](./edit.md)' $mdfiles
 sd '\[The Factory\]\(.+?\)' '[The Factory](./factory.md)' $mdfiles
 sd '\[The Manual\]\(.+?\)' '[The Manual](./manual.md)' $mdfiles
 sd '\[The Migration\]\(.+?\)' '[The Migration](./migration.md)' $mdfiles
-sd '\[The Prototype\]\(.+?\)' '[The Prototype](./prototype.md)' $mdfiles
 sd '\[The Rules\]\(.+?\)' '[The Rules](./rules.md)' $mdfiles
 sd '\[The Show\]\(.+?\)' '[The Show](./show.md)' $mdfiles
 sd '\[The Study\]\(.+?\)' '[The Study](./study.md)' $mdfiles
+
+sd '\[The Prototype\]\(.+?\)' '[The Prototype](./prototype.md)' $mdfiles
+sd '\[The Discovery\]\(.+?\)' '[The Prototype](./prototype.md)' $mdfiles
+
 sd '\[The Widget\]\(.+?\)' '[The Widget](./widget.md)' $mdfiles
+sd '\[The Sample\]\(.+?\)' '[The Widget](./widget.md)' $mdfiles
 
 sd -F \
     'Most projects won’t do all of these steps.
@@ -33,3 +37,13 @@ Choose the ones that make the most sense for your goals, available skills and re
 sd --flags=i -- '^- support roles.*$' \
     '- Support roles, such as community managers, foundation leaders, and fiscal and other sponsors (and sponsor staff, such as finance, recruiters, sysadmins, or legal)' \
     $mdfiles
+
+set found_forbidden 0
+for forbidden in 'The Discovery' 'The Sample'
+    if rg -F $forbidden $mdfiles
+        echo "WARNING FORBIDDEN PHRASE: `$forbidden`"
+        set found_forbidden 1
+    end
+end
+
+exit $found_forbidden
